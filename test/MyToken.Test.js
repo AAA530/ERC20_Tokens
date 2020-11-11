f79288f0b28dd83ef544c9470434b2a4ff794d1d
AAA530 <dhairyapatelad@gmail.com>
2020-11-11 23:52:04 +0530

    writing some tests

--- test/MyToken.Test.js
+++ test/MyToken.Test.js
@@ -11,12 +11,30 @@ chai.use(chaiAsPromise);
 
 const expect = chai.expect;
 contract("Token Test", async (accounts) => {
-  it("all tokens"),
-    async () => {
-      let instance = await Token.deployed();
-      let totalSupply = await instance.totalSupply();
-      expect(
-        await instance.balanceOf(accounts[0])
-      ).to.eventually.be.a.bignumber.equal(totalSupply);
-    };
+  const [deployedAccount, recepientAccount, anotherAccount] = accounts;
+
+  it("all tokens are present", async () => {
+    let instance = await Token.deployed();
+    let totalSupply = await instance.totalSupply();
+    expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(
+      totalSupply
+    );
+  });
+
+  it("Transfer tokens form on to another", async () => {
+    const senToken = 1;
+    let instance = await Token.deployed();
+    let totalSupply = await instance.totalSupply();
+    expect(await instance.balanceOf(deployedAccount)).to.be.a.bignumber.equal(
+      totalSupply
+    );
+    expect(instance.transfer(recepientAccount, senToken)).to.eventually.be
+      .fulfilled;
+    expect(
+      instance.balanceOf(deployedAccount)
+    ).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(senToken)));
+    expect(
+      instance.balanceOf(recepientAccount)
+    ).to.eventually.be.a.bignumber.equal(new BN(senToken));
+  });
 });
