cda5c21d29afb54c81c838d17ba68de8098010df
AAA530 <dhairyapatelad@gmail.com>
2020-11-13 16:12:45 +0530

    adding KYC function in react

--- client/src/App.js
+++ client/src/App.js
@@ -9,52 +9,87 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const tokenInstance = new web3.eth.Contract(
-        MyToken.abi,
-        MyToken.networks[networkId] && MyToken.networks[networkId].address
-      );
-
-      const tokenSalesInstance = new web3.eth.Contract(
-        MyTokenSale.abi,
-        MyTokenSale.networks[networkId] &&
-          MyTokenSale.networks[networkId].address
-      );
-
-      const KycInstance = new web3.eth.Contract(
-        KycContract.abi,
-        KycContract.networks[networkId] &&
-          KycContract.networks[networkId].address
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      setLoaded(true);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
-    }
+  // const [loaded, setLoaded] = useState(false);
+  const [kycAddress, setKycAddress] = useState();
+  const [obj, setObj] = useState({
+    loaded: false,
+    kycAddress: "",
   });
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    async function ConfigureWithTruffle() {
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3();
 
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
+
+        // Get the contract instance.
+        const networkId = await web3.eth.net.getId();
+        const tokenInstance = new web3.eth.Contract(
+          MyToken.abi,
+          MyToken.networks[networkId] && MyToken.networks[networkId].address
+        );
+
+        const tokenSalesInstance = new web3.eth.Contract(
+          MyTokenSale.abi,
+          MyTokenSale.networks[networkId] &&
+            MyTokenSale.networks[networkId].address
+        );
+
+        const kycInstance = new web3.eth.Contract(
+          KycContract.abi,
+          KycContract.networks[networkId] &&
+            KycContract.networks[networkId].address
+        );
+
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        setObj({ ...obj, loaded: true });
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`
+        );
+        console.error(error);
+      }
+    }
+    ConfigureWithTruffle();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    console.log(name);
+    setObj({
+      ...obj,
+      [name]: value,
+    });
+  };
+
+  const handleKycWhiteListing = async () => {
+    await kycInstance.SetKycCompleted(obj.kycAddress);
+  };
+
+  console.log(obj);
+
+  console.log(obj.kycAddress);
   return (
     <div>
-      <p>You clicked times</p>
-      <button>Click me</button>
+      <h1>ERC 20 Tokens of Kasper</h1>
+      <h2>KYC Whitelisting</h2>
+      Enter Your address :
+      <input
+        type="text"
+        name="kycAddress"
+        value={obj.kycAddress}
+        onChange={handleInputChange}
+      />
+      <button type="button" onClick={handleKycWhiteListing}>
+        Set Kyc
+      </button>
     </div>
   );
 }
